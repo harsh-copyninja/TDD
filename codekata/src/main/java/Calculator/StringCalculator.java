6525e05b40462dbe3d169d37ea9d7d7dd34af9c3
Harsh <jain.harsh.bhandari@gmail.com>
2020-06-14 23:17:26 +0530

    Fix for Two Parameters

--- codekata/src/main/java/Calculator/StringCalculator.java
+++ codekata/src/main/java/Calculator/StringCalculator.java
@@ -5,7 +5,10 @@ public class StringCalculator {
 	public int intAdd(String toBeAdded) {
 		if (toBeAdded == "")
 			return 0;
-		return Integer.parseInt(toBeAdded);
+		String nums[] = toBeAdded.split(",");
+		if (nums.length == 1)
+			return Integer.parseInt(nums[0]);
+		return Integer.parseInt(nums[0])+Integer.parseInt(nums[1]);
 	}
 
 }
