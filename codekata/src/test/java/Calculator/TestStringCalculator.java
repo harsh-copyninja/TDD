8248f45d3aabece238904c966d210db92be55426
Harsh <you@example.com>
2020-06-14 21:09:29 +0530

    Setting up repo

---
+++ codekata/src/test/java/Calculator/TestStringCalculator.java
@@ -0,0 +1,13 @@
+package Calculator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestStringCalculator {
+
+	@Test
+	public void setUpTest() {
+		assertEquals(1,1);
+	}
+}
