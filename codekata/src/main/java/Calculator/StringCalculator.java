cf3e6e05699c87ca1cf49db3cdd3616e844691e3
Harsh <jain.harsh.bhandari@gmail.com>
2020-06-15 10:44:01 +0530

    Refratoring for new Delimeter logic

--- codekata/src/main/java/Calculator/StringCalculator.java
+++ codekata/src/main/java/Calculator/StringCalculator.java
@@ -5,17 +5,14 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
+	private final String DELEMITER_REGEX = "//.*\n";
+	
 	public int intAdd(String toBeAdded) {
 		int sum = 0;
 		String del = ",";
-		if (toBeAdded.startsWith("//")) {
-			String regex = "//.*\n";
-			Pattern pattern = Pattern.compile(regex);
-			Matcher m = pattern.matcher(toBeAdded);
-			if(m.find()) {
-				del = toBeAdded.substring(m.start()+2,m.end()-1);
-				toBeAdded = toBeAdded.substring(m.end());
-			}
+		if (isNewDelimeter(toBeAdded)) {
+			del = getNewDelimeter(toBeAdded);
+			toBeAdded = removeDelimeterText(toBeAdded);
 		}
 		toBeAdded = toBeAdded.replaceAll("\n", del);
 		String nums[] = toBeAdded.split(del);
@@ -23,6 +20,24 @@ public class StringCalculator {
 			sum+=parseInt(num);
 		}
 		return sum;
+	}
+	
+	private String removeDelimeterText(String toBeAdded) {
+		return toBeAdded.replaceAll(DELEMITER_REGEX, "");
+	}
+	
+	private String getNewDelimeter(String toBeAdded) {
+		String del = ",";
+		Pattern pattern = Pattern.compile(DELEMITER_REGEX);
+		Matcher m = pattern.matcher(toBeAdded);
+		if(m.find()) {
+			del = toBeAdded.substring(m.start()+2,m.end()-1);
+		}
+		return del;
+	}
+	
+	private Boolean isNewDelimeter(String toBeAdded) {
+		return toBeAdded.startsWith("//");
 	}
 	
 	private int parseInt(String num) {
